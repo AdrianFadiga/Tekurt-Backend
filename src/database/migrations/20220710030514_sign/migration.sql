b12bc6ebca937e939a1557c4fbbbc7e37dcda623
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-10 00:10:26 -0300

    tabela de signos

---
+++ src/database/migrations/20220710030514_sign/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `signs` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sign` VARCHAR(15) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
