5c0c7f7c7a70ce29ea66a2bc2770042c7b29bfff
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-20 09:58:10 -0300

    feat: possivel alteraçao de código na camada de model login

--- src/api/models/login.model.ts
+++ src/api/models/login.model.ts
@@ -2,9 +2,17 @@ import { prisma } from '../../database/prismaClient';
 
 export default class LoginModel {
   public async getByEmailOrUsername(user: string) {
-    const userData = prisma.user.findFirst({ where: {
-      OR: [{ email: user }, { username: user }]
-    } });    
+    const userData = prisma.user.findFirst({
+      where: {
+        OR: [{ email: user }, { username: user }]
+      }
+      // select: {
+      //   username: true,
+      //   id: true,
+      //   email: true,
+      //   password: true
+      // }
+    });    
 
     return userData;
   }
