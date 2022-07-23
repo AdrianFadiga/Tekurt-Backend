2c69b0e9412b61e5b0bb5030f944b305a5aa1cb6
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-23 20:07:14 -0300

    feat: removendo o diretorio build da aplicação

--- build/api/schemas/loginSchema.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=loginSchema.js.map
--- build/database/seeders/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./drinking");
-require("./sign");
-require("./socialStatus");
-require("./user");
-//# sourceMappingURL=index.js.map
