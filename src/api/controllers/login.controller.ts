54fdd75010de113a9df93c3e240e05389ac4cfc5
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-23 16:55:23 -0300

    feat: login controller feito em tdd

--- src/api/controllers/login.controller.ts
+++ src/api/controllers/login.controller.ts
@@ -4,11 +4,11 @@ import LoginService from '../services/login.service';
 export default class LoginController {
   constructor(private service: LoginService) {}
 
-  public async getByEmailOrUsername(req: Request, res: Response) {
-    const { user } = req.body;
+  public async sigIn(req: Request, res: Response) {
+    const { user, password } = req.body;
 
-    const userData = await this.service.getByEmailOrUsername(user);
+    const token = await this.service.sigIn(user, password);
 
-    return res.status(200).json(userData);
+    return res.status(200).json({ token });
   }
 }
