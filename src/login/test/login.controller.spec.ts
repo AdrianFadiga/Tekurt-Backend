b62e147581573f76e1da53c64b71fde15de1af2d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-08-14 20:36:18 -0300

    test: testes da controller se retorna um token

--- src/login/test/login.controller.spec.ts
+++ src/login/test/login.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginController } from '../login.controller';
+import { LoginService } from '../login.service';
+import { loginUser, token } from './mocks';
 
 describe('LoginController', () => {
   let loginController: LoginController;
+  let loginService: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LoginController],
+      providers: [
+        {
+          provide: LoginService,
+          useValue: {
+            signIn: jest.fn().mockResolvedValue(token),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     loginController = module.get<LoginController>(LoginController);
+    loginService = module.get<LoginService>(LoginService);
   });
 
-  it('should be defined', () => {
+  it('Deve ser definido', () => {
     expect(loginController).toBeDefined();
+    expect(loginService).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('Deve retornar um token na "access_controll"', async () => {
+      const result = await loginController.signIn(loginUser);
+
+      expect(loginService.signIn).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(token);
+    });
   });
 });
---
+++ src/login/test/mocks/loginUser.ts
@@ -0,0 +1,3 @@
+import { AuthDto } from './../../dtos/authDto.dto';
+
+export const loginUser = new AuthDto('usuario teste', '12345678');
