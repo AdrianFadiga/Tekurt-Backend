593e4b2081fb781161a35ed461678a4af9c7fc5d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-08-14 21:24:30 -0300

    test: teste da sigIn service em casos de sucesso

--- src/login/test/login.service.spec.ts
+++ src/login/test/login.service.spec.ts
@@ -1,18 +1,55 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { LoginModel } from '../login.model';
 import { LoginService } from '../login.service';
+import { loginModelResponse, loginUser, token } from './mocks';
+import * as bcrypt from 'bcrypt';
 
 describe('LoginService', () => {
-  let service: LoginService;
+  let loginService: LoginService;
+  let loginModel: LoginModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoginService],
+      providers: [
+        LoginService,
+        {
+          provide: LoginModel,
+          useValue: {
+            signIn: jest.fn().mockResolvedValue(loginModelResponse),
+            findByEmailOrUsername: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+        JwtService,
+        ConfigService,
+      ],
     }).compile();
 
-    service = module.get<LoginService>(LoginService);
+    loginService = module.get<LoginService>(LoginService);
+    loginModel = module.get<LoginModel>(LoginModel);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Deve ser definido', () => {
+    expect(loginService).toBeDefined();
+    expect(loginModel).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    describe('Em casos de sucesso', () => {
+      it('Deve retornar um token em "access_token"', async () => {
+        jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(true);
+        const result = await loginService.signIn(loginUser);
+
+        expect(loginModel.signIn).toHaveBeenCalledTimes(1);
+        expect(result).toHaveProperty('access_token');
+        expect(typeof result.access_token).toBe('string');
+      });
+    });
+
+    describe('Em casos de erro', () => {
+      it('Deve retornar um erro caso o usuário não exista', () => {});
+    });
   });
 });
--- src/login/test/mocks/user.ts
+++ src/login/test/mocks/user.ts
@@ -3,6 +3,13 @@ import { AuthDto } from '../../dtos/authDto.dto';
 
 export const loginUser = new AuthDto('usuario teste', '12345678');
 
+export const loginModelResponse = {
+  username: 'usaurio',
+  email: 'teste@gmail.com',
+  id: 'id_aleatorio',
+  password: 'senha123',
+};
+
 export const createUser: UserDto = {
   firstName: 'usuario teste',
   lastName: 'sobrenome',
