cedf74825c36fd9b5331367afca4c43c250a2730
AdrianFadiga <adrianmfadiga@gmail.com>
2022-08-23 22:18:07 -0300

    feat: CRUD da rota posts finalizado

--- src/post/post.model.ts
+++ src/post/post.model.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
+import { PostDto } from './dtos';
 
 @Injectable()
 export class PostModel {
@@ -17,5 +18,26 @@ export class PostModel {
       where: { id },
     });
     return post;
+  }
+
+  async create(authorId: string, dto: PostDto) {
+    const newPost = await this.database.post.create({
+      data: { ...dto, authorId },
+    });
+    return newPost;
+  }
+
+  async update(postId: string, dto: PostDto) {
+    const updatedPost = await this.database.post.update({
+      where: { id: postId },
+      data: { ...dto, updatedAt: new Date() },
+    });
+    return updatedPost;
+  }
+
+  async delete(postId: string) {
+    await this.database.post.delete({
+      where: { id: postId },
+    });
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { PostDto } from './dtos';
 import { PostModel } from './post.model';
 
 @Injectable()
@@ -15,9 +20,29 @@ export class PostService {
     return this.postModel.readAll(id);
   }
 
+  async verifyPostAuthor(tokenId: string, authorId: string) {
+    if (tokenId !== authorId) throw new UnauthorizedException('Unauthorized');
+  }
+
   async readOne(id: string) {
     const post = await this.postModel.readOne(id);
     if (!post) throw new NotFoundException('Post not found');
     return post;
+  }
+
+  async create(authorId: string, dto: PostDto) {
+    return this.postModel.create(authorId, dto);
+  }
+
+  async update(postId: string, id: string, dto: PostDto) {
+    const { authorId } = await this.readOne(postId);
+    await this.verifyPostAuthor(id, authorId);
+    return this.postModel.update(postId, dto);
+  }
+
+  async delete(postId: string, id: string) {
+    const { authorId } = await this.readOne(postId);
+    await this.verifyPostAuthor(id, authorId);
+    await this.postModel.delete(postId);
   }
 }
